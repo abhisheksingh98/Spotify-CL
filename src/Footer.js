ef3484d115850acc99470e5238eccde63d0b5690
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-11-19 21:14:31 +0530

    completed Sidebar and Footer Section

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import "./Footer.css"
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline"
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious"
+import SkipNextIcon from "@material-ui/icons/SkipNext"
+import ShuffleIcon from "@material-ui/icons/Shuffle"
+import RepeatIcon from "@material-ui/icons/Repeat"
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay'
+import VolumeDownIcon from '@material-ui/icons/VolumeDown'
+import { Grid, Slider } from '@material-ui/core'
 
 function Footer() {
     return (
         <div className = "footer">
-            <h1>I am the Footer</h1>
+            <div className="footer__left">
+                <img className = "footer__albumLogo" src="https://m.media-amazon.com/images/I/91xYIYDd-5L._SS500_.jpg" alt=""/>
+                <div className="footer__songInfo">
+                    <h4>YEAH!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+
+            <div className="footer__center">
+            <ShuffleIcon className = "footer__green" />
+            <SkipPreviousIcon className = "footer__icon" />
+            <PlayCircleOutlineIcon fontSize="large" className = "footer__icon" />
+            <SkipNextIcon className="footer__icon" />
+            <RepeatIcon className = "footer__green" />
+            </div>
+
+            <div className="footer__right">
+            <Grid container spacing = {2}>
+            <Grid item>
+            <PlaylistPlayIcon/>
+            </Grid>
+            <Grid item>
+            <VolumeDownIcon/>
+            </Grid>
+            <Grid item xs>
+            <Slider/>
+            </Grid>
+            </Grid>
+            </div>
+
+            <div className="footer__right">
+            </div>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false,
     item: null,
     //Remove the token after finishing development
-    token : "null"
+    token : "BQBtKnWUq4snhz6onCphL0IJBagV-3ni-keOlZTqum8ZW3U4Ozr024ONsbQXIyLnQuaD8wy5SAQ3tsg43Dt_HBoa1o7IuMjSFGH_5Kfy-wFJnoW_ub1baBHv5K1n8WlGc-lY3TII-6APH8nZQ5p9TOH24e5UkkxPaYJZYiariSPJcEJw"
 }
 
 const reducer = (state, action) => {
@@ -22,6 +22,11 @@ const reducer = (state, action) => {
                     ...state,
                     token: action.token
                 }
+            case 'SET_PLAYLISTS':
+                return {
+                    ...state,
+                    playlists: action.playlists,
+                }        
             default:
                 return state;
     }
